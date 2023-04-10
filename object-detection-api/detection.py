ec0c432a890bb1beea0706d8c4f19156c2895e3d
Wut Hmone Hnin Hlaing @ Alex Snow <alexsnow348@gmail.com>
2023-04-10 10:41:34 +0200

    first commit

---
+++ object-detection-api/detection.py
@@ -0,0 +1,11 @@
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import mlflow
+
+user = 'root'
+pwd = 'Yonyamin@10'
+hostname = '127.0.0.1'
+port = 3306
+database = 'mlflow'
+uri = 'mysql://{user}:{password}@{hostname}:{port}/{databse}'
+mlflow.set_tracking_uri(uri)
